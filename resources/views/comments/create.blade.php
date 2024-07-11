4bda1ef95d1708e64ebb76519a268b28fad83892
Tarek Yaghi <you@example.com>
2024-07-11 10:15:30 +0300

    update session message view

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -15,6 +15,16 @@
     <x-slot:name>Create new Comment for {{ $post->user->name }}'s Post </x-slot>
 
 
+    @if (session('message'))
+        <div id="session_message" class="flex items-center rounded-xl bg-blue-500 text-white text-sm font-bold px-4 py-3"
+            role="alert">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path
+                    d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+            </svg>
+            <p>{{ session('message') }}</p>
+        </div>
+    @endif
 
     <div class="mt-4 mb-4">
         @foreach ($comments as $comment)
@@ -46,13 +56,6 @@
         </div>
     </div>
 
-    @if (session('message'))
-        <div class="mt-2 text-red-500 flex items-center justify-start" id="session_message">
-            <span class="font-bold size-20 text-nowrap">
-                {{ session('message') }}
-            </span>
-        </div>
-    @endif
 
 
     <form method="POST" action="{{ route('comment.store') }}" enctype="multipart/form-data">
